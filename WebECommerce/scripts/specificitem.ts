acaaf0f7e0ec24efda923e4086710a8f5b9a7a58
Ali Küçükkaya <preoyuncu@hotmail.com>
2023-07-13 12:47:49 +0300

    #4

--- WebECommerce/scripts/specificitem.ts
+++ WebECommerce/scripts/specificitem.ts
@@ -1,6 +1,101 @@
-﻿function Bomba() {
-    console.log('bomba');
+﻿class CartList {
+    itemName: string[];
+    itemPrice: string[];
+
+    constructor() {
+        this.itemName = [];
+        this.itemPrice = [];
+    }
 }
+
+let cl: CartList = new CartList();
+
+function loadCartListFromLocalStorage() {
+    const storedCartList = localStorage.getItem('cartList');
+    if (storedCartList) {
+        cl = JSON.parse(storedCartList);
+    }
+}
+
+function saveCartListToLocalStorage() {
+    localStorage.setItem('cartList', JSON.stringify(cl));
+}
+function loadCartListIntoCartListDiv() {
+    const storedCartList = localStorage.getItem('cartList');
+    const cartListDiv = document.querySelector('#cartListDiv'); // Assuming you have a div element with the id "cartListDiv"
+
+    if (storedCartList) {
+        const cl: CartList = JSON.parse(storedCartList);
+        const itemCounts: { [itemName: string]: number } = {}; // Object to keep track of item counts
+        const itemPrices: { [itemName: string]: number } = {}; // Object to keep track of item prices
+        let totalPrice = 0; // Variable to store the total price
+
+        // Calculate item counts, total prices, and the total price of all items
+        cl.itemName.forEach((itemName, index) => {
+            if (itemCounts[itemName]) {
+                itemCounts[itemName]++;
+                itemPrices[itemName] += parseFloat(cl.itemPrice[index]);
+            } else {
+                itemCounts[itemName] = 1;
+                itemPrices[itemName] = parseFloat(cl.itemPrice[index]);
+            }
+            totalPrice += parseFloat(cl.itemPrice[index]);
+        });
+
+        // Render the items with their counts and total prices
+        for (let i = 0; i < cl.itemName.length; i++) {
+            const itemName = cl.itemName[i];
+
+            // Skip rendering if the item has already been rendered
+            if (i !== cl.itemName.indexOf(itemName)) {
+                continue;
+            }
+
+            const itemContainer = document.createElement('div');
+            const itemInfoElement = document.createElement('p');
+
+            const itemCount = itemCounts[itemName];
+            const totalPriceForItem = itemPrices[itemName];
+
+            itemInfoElement.textContent = `${itemName} (${itemCount}) - $${totalPriceForItem.toFixed(2)}`;
+
+            itemContainer.appendChild(itemInfoElement);
+            cartListDiv.appendChild(itemContainer);
+        }
+
+        // Render the total price of all items
+        const totalPriceElement = document.createElement('p');
+        totalPriceElement.textContent = `Total Price: $${totalPrice.toFixed(2)}`;
+        cartListDiv.appendChild(totalPriceElement);
+    }
+}
+function ClearCartList() {
+    localStorage.removeItem('cartList');
+}
+
+function SendOrder() {
+}
+
+function AddToCart() {
+    var itemNameTag = document.querySelector('.itemBox'); // Assuming the itemBox class is used for the item name element
+    var itemPriceTag = document.querySelector('.itemPrice'); // Assuming the itemPrice class is used for the item price element
+
+    var itemNames = itemNameTag.textContent;
+    var itemPrice = itemPriceTag.textContent;
+    cl.itemName.push(itemNames); // Add the item name to the itemName array of CartList
+    cl.itemPrice.push(itemPrice); // Add the item price to the itemPrice array of CartList
+
+    var formattedItemName = cl.itemName.join(", "); // Join the item names with commas and a space
+    var formattedItemPrice = cl.itemPrice.join(", "); // Join the item prices with commas and a space
+
+    console.log(formattedItemName); // Output the formatted item names
+    console.log(formattedItemPrice); // Output the formatted item prices
+
+    saveCartListToLocalStorage(); // Save the updated CartList to localStorage
+}
+
+loadCartListFromLocalStorage(); // Load the CartList from localStorage when the page loads
+
 
 function GetTheItem() {
     const urlParams = new URLSearchParams(window.location.search);
@@ -33,6 +128,7 @@ function GetTheItem() {
 
                 var itemPrice = document.createElement("p");
                 itemPrice.classList.add('itemPrice');
+                itemPrice.setAttribute('id', 'itemPrice');
                 itemPrice.textContent = item.itemPrice;
                 itemDiv.appendChild(itemPrice);
 
@@ -49,6 +145,6 @@ function GetTheItem() {
 
 const addToCartButton = document.getElementById('addtoCart') as HTMLButtonElement;
 if (addToCartButton !== null) {
-    addToCartButton.addEventListener("click", Bomba, false);
+    addToCartButton.addEventListener("click", AddToCart, false);
 }
 
--- WebECommerce/wwwroot/js/specificitem.js
+++ WebECommerce/wwwroot/js/specificitem.js
@@ -1,6 +1,80 @@
-function Bomba() {
-    console.log('bomba');
+var CartList = /** @class */ (function () {
+    function CartList() {
+        this.itemName = [];
+        this.itemPrice = [];
+    }
+    return CartList;
+}());
+var cl = new CartList();
+function loadCartListFromLocalStorage() {
+    var storedCartList = localStorage.getItem('cartList');
+    if (storedCartList) {
+        cl = JSON.parse(storedCartList);
+    }
 }
+function saveCartListToLocalStorage() {
+    localStorage.setItem('cartList', JSON.stringify(cl));
+}
+function loadCartListIntoCartListDiv() {
+    var storedCartList = localStorage.getItem('cartList');
+    var cartListDiv = document.querySelector('#cartListDiv'); // Assuming you have a div element with the id "cartListDiv"
+    if (storedCartList) {
+        var cl_1 = JSON.parse(storedCartList);
+        var itemCounts_1 = {}; // Object to keep track of item counts
+        var itemPrices_1 = {}; // Object to keep track of item prices
+        var totalPrice_1 = 0; // Variable to store the total price
+        // Calculate item counts, total prices, and the total price of all items
+        cl_1.itemName.forEach(function (itemName, index) {
+            if (itemCounts_1[itemName]) {
+                itemCounts_1[itemName]++;
+                itemPrices_1[itemName] += parseFloat(cl_1.itemPrice[index]);
+            }
+            else {
+                itemCounts_1[itemName] = 1;
+                itemPrices_1[itemName] = parseFloat(cl_1.itemPrice[index]);
+            }
+            totalPrice_1 += parseFloat(cl_1.itemPrice[index]);
+        });
+        // Render the items with their counts and total prices
+        for (var i = 0; i < cl_1.itemName.length; i++) {
+            var itemName = cl_1.itemName[i];
+            // Skip rendering if the item has already been rendered
+            if (i !== cl_1.itemName.indexOf(itemName)) {
+                continue;
+            }
+            var itemContainer = document.createElement('div');
+            var itemInfoElement = document.createElement('p');
+            var itemCount = itemCounts_1[itemName];
+            var totalPriceForItem = itemPrices_1[itemName];
+            itemInfoElement.textContent = "".concat(itemName, " (").concat(itemCount, ") - $").concat(totalPriceForItem.toFixed(2));
+            itemContainer.appendChild(itemInfoElement);
+            cartListDiv.appendChild(itemContainer);
+        }
+        // Render the total price of all items
+        var totalPriceElement = document.createElement('p');
+        totalPriceElement.textContent = "Total Price: $".concat(totalPrice_1.toFixed(2));
+        cartListDiv.appendChild(totalPriceElement);
+    }
+}
+function ClearCartList() {
+    localStorage.removeItem('cartList');
+}
+function SendOrder() {
+}
+function AddToCart() {
+    var itemNameTag = document.querySelector('.itemBox'); // Assuming the itemBox class is used for the item name element
+    var itemPriceTag = document.querySelector('.itemPrice'); // Assuming the itemPrice class is used for the item price element
+    var itemNames = itemNameTag.textContent;
+    var itemPrice = itemPriceTag.textContent;
+    cl.itemName.push(itemNames); // Add the item name to the itemName array of CartList
+    cl.itemPrice.push(itemPrice); // Add the item price to the itemPrice array of CartList
+    var formattedItemName = cl.itemName.join(", "); // Join the item names with commas and a space
+    var formattedItemPrice = cl.itemPrice.join(", "); // Join the item prices with commas and a space
+    console.log(formattedItemName); // Output the formatted item names
+    console.log(formattedItemPrice); // Output the formatted item prices
+    saveCartListToLocalStorage(); // Save the updated CartList to localStorage
+}
+loadCartListFromLocalStorage(); // Load the CartList from localStorage when the page loads
 function GetTheItem() {
     var urlParams = new URLSearchParams(window.location.search);
     var itemName = urlParams.get('itemName');
@@ -28,6 +102,7 @@ function GetTheItem() {
             itemDiv.appendChild(itemPara);
             var itemPrice = document.createElement("p");
             itemPrice.classList.add('itemPrice');
+            itemPrice.setAttribute('id', 'itemPrice');
             itemPrice.textContent = item.itemPrice;
             itemDiv.appendChild(itemPrice);
             var itemPriceTag = document.createElement("p");
@@ -42,6 +117,6 @@ function GetTheItem() {
 }
 var addToCartButton = document.getElementById('addtoCart');
 if (addToCartButton !== null) {
-    addToCartButton.addEventListener("click", Bomba, false);
+    addToCartButton.addEventListener("click", AddToCart, false);
 }
 //# sourceMappingURL=specificitem.js.map
